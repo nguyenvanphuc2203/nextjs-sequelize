4cc1390bb0a3a22e8a8a83f75666d06379af17de
Nguyễn Phúc <macos@MacBook-Pro.local>
2020-11-29 21:39:05 +0700

    add script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+npm install --save-dev sequelize-cli &
+npx sequelize db:migrate &
+npx sequelize db:seed:all &
+
 if [ "$NODE_ENV" == "production" ] ; then
-  npm install --save-dev sequelize-cli &
-  npx sequelize db:migrate &
-  npx sequelize db:seed:all &
   npm run start
 else
   npm run dev
